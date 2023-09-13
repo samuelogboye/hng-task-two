3108604b3590691e7844f4b68d1bf9de94555d94
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-13 21:51:00 +0100

    Code refractor to securely manage db

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql10646166:mgmaMA1dSl@sql10.freesqldatabase.com/sql10646166'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 
 db = SQLAlchemy(app)
 
